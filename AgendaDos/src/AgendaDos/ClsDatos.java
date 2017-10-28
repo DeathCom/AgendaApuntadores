c99ef1cffd000a3f2221ac02bddd9a73f216dc43
Yogg-Saron <www-rainbow_tables@outlook.com>
2017-10-28 07:42:15 -0600

    Add Codigo

---
+++ AgendaDos/src/AgendaDos/ClsDatos.java
@@ -0,0 +1,27 @@
+
+package AgendaDos;
+
+public class ClsDatos {
+   ClsContacto ObjetoInicial;
+   ClsContacto ObjetoCola;
+   
+   public ClsContacto llenarObjeto(){
+       return ObjetoInicial;
+   }
+   public void guardar(){
+       
+   }
+   public ClsContacto sigueinte(){
+       return ObjetoInicial;
+   }
+   public ClsContacto anterior(){
+       return ObjetoInicial;
+   }
+   public ClsContacto primero(){
+       return ObjetoInicial;
+   }
+   public ClsContacto utimo(){
+       return ObjetoInicial;
+   }
+   
+}
